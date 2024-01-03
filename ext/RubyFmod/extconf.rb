14a7ebde4100b5f99f3b4affdec49af23a38be17
Nuri Yuri <yuri@pokemonworkshop.com>
2024-01-03 17:01:56 +0100

    Make it able to build under MacOS

--- ext/RubyFmod/extconf.rb
+++ ext/RubyFmod/extconf.rb
@@ -1,5 +1,11 @@
 require 'mkmf'
 ext_name = 'RubyFmod'
+
+if Dir.exist?('/opt/homebrew')
+    $INCFLAGS << " -I'/opt/homebrew/include'"
+    $LDFLAGS << " -L'/opt/homebrew/lib'"
+end
+
 have_library('fmod')
 
 create_makefile(ext_name)
